2eaf47a32f4e14efcdf890329040db3fb5fabf1c
pvdnat <pducv1192000@gmail.com>
2023-02-08 13:58:14 -0500

    adding comments and testing using own created text file

--- src/wish.c
+++ src/wish.c
@@ -5,16 +5,35 @@
 #include <ctype.h>
 #include "wish.h"
 
-int main(int argc, char *argv[])
-{
-  // These two lines make the macOS C compiler happy.
-  // Otherwise, it complains about unused parameters.
-  argc = 0;
-  argv = NULL;
-  
-  // This is just a skeleton for your convenience
-  wish_read_line(stdin);
-  wish_read_config("wish.config", 1);
-  
-  return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+    // These two lines make the macOS C compiler happy.
+    // Otherwise, it complains about unused parameters.
+    argc = 0;
+    argv = NULL;
+
+    // This is just a skeleton for your convenience
+    //wish_read_line(stdin);
+    //wish_read_config("wish.config", 1);
+
+
+    printf("%s", WISH_DEFAULT_PROMPT);
+
+    if (wish_read_line(stdin)!=NULL) {
+        printf("%s\n", "Success!!!");
+        return EXIT_SUCCESS;
+    } else {
+        printf("%s\n", "Fail!!!");
+        return EXIT_FAILURE;
+    }
+    
+    /*
+    if (wish_read_config("text.txt", 1)==0) {
+        printf("%s\n", "Success!!!");
+        return EXIT_SUCCESS;
+    } else {
+        printf("%s\n", "Fail!!!");
+        return EXIT_FAILURE;
+    }
+    */
+
 }
--- src/wish_read.c
+++ src/wish_read.c
@@ -7,54 +7,65 @@
 
 
 char *wish_read_line(FILE *in) {
-
-  int buffer_length = 2048, count = 0;
-  char *buffer = (char *)malloc(sizeof(char) * buffer_length);
-  if (buffer==NULL) {
-    perror("Error: ");
-  }
-
-  char str[WISH_MAX_INPUT];
-
-  while (fgets(str, WISH_MAX_INPUT, in) != NULL) {
-    if (count == buffer_length) {
-      buffer_length *= 2;
-      buffer = realloc(buffer, buffer_length);
-      if (buffer == NULL) {
-        perror("Error: ");
-      }
-    }
-
-    if (strchr(str, '\n') != NULL) {
-      if (!isspace((int)*str)) {
-        str[strcsp(str, '\n')] = 0;
-        strcpy((buffer+count), str);
-        count++;
-      } else {
+    // Define buffer length and buffer counter 
+    int buffer_length = 2048, count = 0;
+    char *buffer = (char *)malloc(sizeof(char) * buffer_length);
+    if (buffer==NULL) {
+        perror("wish error: fail to allocate buffer memory");
         return NULL;
-      }
-    } else {
-      while (strchr(str, '\n') == NULL) {
-        fgets(str, WISH_MAX_INPUT, in);
-      }
-      printf("wish: line too long");
-      fgets(str, WISH_MAX_INPUT, in);
-      return NULL;
     }
-  }
 
-  return buffer;
+    // Define string pointer */
+    char str[WISH_MAX_INPUT];
+
+    // Get the first line from the file and check if line exist 
+    while (fgets(str, WISH_MAX_INPUT, in) != NULL) {
+
+        // Check if buffer has enough memory and reallocate if needed 
+        if (count == buffer_length) {
+            buffer_length *= 2;
+            buffer = realloc(buffer, buffer_length);
+            if (buffer == NULL) {
+                perror("wish error: fail to reallocate buffer memory");
+                return NULL;
+            }
+        }
+
+
+        // Check if end of line 
+        if (strchr(str, '\n') != NULL) {
+            if (!isspace((int)*str)) {            // Check if line is include empty 
+                str[strcspn(str, "\n")] = 0;         // Remove newline character
+                strcpy((buffer+count), str);
+                count++;
+            } else {
+                return NULL;
+            }
+
+        // Discard the whole line that longer than max input 
+        } else {
+            while (strchr(str, '\n') == NULL) {   // Keep discarding until end of line 
+                fgets(str, WISH_MAX_INPUT, in);
+            }
+            fprintf(stderr, "%s", "wish error: line too long\n");
+            return NULL;
+        }
+    }
+    return buffer;
 }
 
 int wish_read_config(char *fname, int ok_if_missing) {
-  FILE *f;
-  f = fopen(fname, 'r');
-  if (f == NULL && ok_if_missing != 1) {
-    perror("Error: ");
-    return 0;
-  } else if (f!=NULL) {
-    wish_read_line(f );
+    // Check if file exist and read it 
+    FILE *f;
+    f = fopen(fname, "r");
+    if (f == NULL && ok_if_missing != 1) {
+        perror("wish error: file not exists");
+        return 1;
+    } else if (f!=NULL) {
+        if (wish_read_line(f) != NULL) {
+            return 0;
+        }
+    }
+    fclose(f);
     return 1;
-  }
-  fclose(f);
 }
