39d4805ca28962572e98d8752a07498d23bd0c29
Dmitry <dzinoviev@gmail.com>
2023-04-11 23:37:24 -0400

    Code for HW5

--- src/wish_parse.c
+++ src/wish_parse.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include "wish.h"
-#include <string.h>
+
 // https://en.wikipedia.org/wiki/Escape_sequences_in_C#Table_of_escape_sequences
 char *wish_unquote(char * s) {
   int i, j;
@@ -38,29 +38,19 @@ void yyerror(const char* s) {
 }
 
 char *wish_safe_getenv(char *name) {
-  char* env = getenv(name);
-  if (!env) {
-    env = super_strdup("");
-  }
-  return env;
+  static char *empty = "";
+  char *var = getenv(name);  
+  if (var)
+    return var;
+  else
+    return empty;
 }
 
 void wish_assign(char *name, char *value) {
-  /* If name does exist in the environment, then its value
-   * is changed to value if overwrite is nonzero;
-   * The setenv() function returns zero on success,
-   * or -1 on error, with errno set to indicate the cause of the error.
-   */
-
-  //char* env = wish_safe_getenv(name);
-  if (strcmp(wish_safe_getenv(name),"")==0) {
-    setenv(name, value, 0);
-  } else {
-    setenv(name, value, 1);
-  }
-
-  if (name) free(name);
-  if (value) free(value);
+  if(setenv(name, value, 1))
+    perror(name);
+  free(name);
+  free(value);
 }
 
 // Find the first program on the command line
@@ -98,17 +88,44 @@ prog_t *create_program(arglist_t al)
 
 int handle_child(pid_t pid, int bgmode)
 {
-  int status;
-  char str[10];
-  if (bgmode==0) {
-    if (waitpid(pid, &status,0)==-1) {
+  if(!bgmode) {
+    int wstatus;
+    if (-1 == waitpid(pid, &wstatus, 0)) {
       perror("waitpid");
       return 1;
     }
-    sprintf(str, "%d", WEXITSTATUS(status));
-    setenv("_",str,1);
+    
+    if (WIFEXITED(wstatus)) {
+      char value[16];
+      snprintf(value, sizeof value, "%d", WEXITSTATUS(wstatus));
+      if(setenv("_", value, 1))
+	return 1;
+    } else
+      return 1;    
   }
   return 0;
+}
+
+// "Rename" fule descriptor "old" to "new," if necessary. After the
+// execution of this function a program that "believes" that it uses
+// the "old" descriptor (e.g., stdout #1 for output) will be actually
+// using the "new" descriprot (e.g., an outgoinf pipe).  This
+// functions terminates the process of error and should not be used in
+// the parent, only in a child.
+static void dup_me (int new, int old) {
+  if (new != old && -1 == dup2(new, old)) {
+    perror("dup2");
+    exit(1);
+  }
+}
+
+// Execute the program defined in "exe"
+static void start(prog_t *exe) {
+  arglist_t args = exe->args;
+  args.args = super_realloc(args.args, sizeof(char*) * (args.size + 1));
+  args.args[args.size] = (char*)NULL;
+  execvp(args.args[0], args.args);
+  perror(args.args[0]);
 }
 
 int spawn(prog_t *exe, int bgmode)
@@ -132,9 +149,7 @@ int spawn(prog_t *exe, int bgmode)
     return 1;
     
   case 0: // Child
-    exe->args = add_to_arglist(exe->args, NULL);
-    execvp(exe->args.args[0], exe->args.args);
-    perror(exe->args.args[0]);
+    start(exe);
     _exit(EXIT_FAILURE); // Do NOT use exit()!
     
   default: // Parent
@@ -142,11 +157,27 @@ int spawn(prog_t *exe, int bgmode)
   }
 }
 
+// Find the number of programs on the command line
+static size_t cmd_length(prog_t *exe) {
+  int count = 0;
+  while(exe) {
+    exe = exe->prev;
+    count++;
+  }
+  return count;
+}
+
 void free_memory(prog_t *exe)
 {
   for(int i = 0; i < exe->args.size; i++)
     free(exe->args.args[i]);
   free(exe->args.args);
+  if(exe->redirection.in)
+    free(exe->redirection.in);
+  if(exe->redirection.out1)
+    free(exe->redirection.out1);
+  if(exe->redirection.out2)
+    free(exe->redirection.out2);
   free (exe);
 }
 
